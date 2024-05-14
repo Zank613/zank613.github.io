3f527c4055de30d056e13a34f0b5b03bafaae204
Ataerk YILDIRIM <114510382+Zank613@users.noreply.github.com>
2024-05-14 19:58:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,41 +16,6 @@ document.addEventListener('DOMContentLoaded', () =>
     document.getElementById('theme-select').value = currentTheme;
 });
 
-// Expand/Collapse project and log details, allowing only one to be open at a time
-document.addEventListener('DOMContentLoaded', () => 
-{
-    const expandableSections = document.querySelectorAll('.project, .log');
-    
-    expandableSections.forEach(section => 
-    {
-        const title = section.querySelector('h3');
-        const details = section.querySelector('p');
-        details.style.display = 'none'; // Initially hide details
-
-        title.addEventListener('click', () => 
-        {
-            // Collapse any currently open sections
-            expandableSections.forEach(sec => 
-            {
-                if (sec !== section) 
-                {
-                    sec.querySelector('p').style.display = 'none';
-                }
-            });
-
-            // Toggle the clicked section
-            if (details.style.display === 'none') 
-            {
-                details.style.display = 'block';
-            } 
-            else 
-            {
-                details.style.display = 'none';
-            }
-        });
-    });
-});
-
 // Theme toggle functionality
 document.getElementById('theme-select').addEventListener('change', (event) => 
 {
